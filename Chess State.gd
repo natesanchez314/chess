f5ef787bf2e03fcda3a9ed3a44ff259d0274151f
Nate Sanchez <30782279+natesanchez314@users.noreply.github.com>
2022-01-26 12:54:27 -0600

    Initial commit

---
+++ Chess State.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var chessState = [
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null]
+]
+
+func _ready():
+	pass
